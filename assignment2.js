16f25e1273bdd50eebdc94983e2c248d1ee9f067
hamsaana <hamsaanaselvan@gmail.com>
2021-12-14 21:17:37 -0500

    1st stage JS not fully functional

---
+++ assignment2.js
@@ -0,0 +1,12 @@
+// creat var to hold thing that is going to be clicked
+const hamburgerMenu = document.querySelector('#burgerMenu');
+
+// listen for click 
+hamburgerMenu.addEventListener(`click`, function() {
+    // create var to hold nav menu
+    const slideOutNav = document.querySelector('#navMenu');
+    // create toggle so that navBar slides in and out when clicked
+    slideOutNav.classList.toggle(`slideOut`);
+});
+
+// hamburgerMenu();
